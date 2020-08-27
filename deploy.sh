671f7f834fdffe91a46a09cf7b77d8b0d8aef245
Mihai <predamihaidragos@gmail.com>
2020-08-27 15:26:36 +0000

    Address comments

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,14 @@
 # testingId is random to avoid multiple people deploying same version at the same time.
 testingId=$(( RANDOM ))
 versionName="testing-$testingId"
-eval "mvn package appengine:deploy clean -Dapp.deploy.version=$versionName -Dapp.deploy.promote=False"
+mvn package appengine:deploy clean -Dapp.deploy.version=$versionName -Dapp.deploy.promote=False
 
 # Run end-to-end tests.
 failUrls=0
 successUrls=0
-for script in end-to-end-tests/*.sh; do
-  if ! sh $script "http://$versionName-dot.photoblur.ew.r.appspot.com/" -H; 
+for script in end-to-end-tests/*.sh
+do
+  if ! $script "http://$versionName-dot.photoblur.ew.r.appspot.com/" -H
   then 
     echo "URL failed: $script" 
     ((failUrls++))
@@ -20,15 +21,15 @@ for script in end-to-end-tests/*.sh; do
 done
 
 # Delete the testing app.
-eval "gcloud app versions delete $versionName --quiet"
+gcloud app versions delete $versionName --quiet
 
 # Log results
 totalUrls=$((failUrls+successUrls))
 echo "URLs: $totalUrls; Success: $successUrls; Failed: $failUrls"
 
 # If all tests were run successfully, deploy the app.
-if [ $failUrls = 0 ];
-  then
+if [ $failUrls -eq 0 ]
+then
   # Change version to the one you want to deploy on.
-  eval "mvn package appengine:deploy clean -Dapp.deploy.version=1"
+  mvn package appengine:deploy clean -Dapp.deploy.version=1
 fi
--- end-to-end-tests/get-blur-areas.sh
+++ end-to-end-tests/get-blur-areas.sh
@@ -1,25 +1,37 @@
+#!/bin/bash
+
+failTests=0
+successTests=0
+
 # Makes a POST request with an image as parameter and compares the response with a file's content.
 # First parameter is the host to make the request to.
 # Second parameter is image's name.
 # Third parameter is the name of the file containing the expected output.
 check_image() {
+  if [ $# -ne 3 ]
+  then
+    echo "The function check_image must receive 3 parameters!"
+    return 1
+  fi
   echo "get-blur-areas: Running test for image: $2"
 
   # Get the blobstore upload url.
-  uploadUrl=`curl "$1blobstore-upload-url?forwardurl=/get-blur-areas"`
+  uploadUrl=$(curl "$1blobstore-upload-url?forwardurl=/get-blur-areas")
 
   # POST the image.
-  response=`curl -X POST -H "Content-Type: multipart/form-data" -F "image=@end-to-end-tests/files/$2" "$uploadUrl"`
+  response=$(curl -X POST -H "Content-Type: multipart/form-data" -F "image=@end-to-end-tests/files/$2" "$uploadUrl")
 
   # Compare the response we got with the expected one.
   # If they are different, fail the test.
-  expected=`cat end-to-end-tests/expected/get-blur-areas/$3`
-  if [ "$response" != "$expected" ];
+  expected=$(cat end-to-end-tests/expected/get-blur-areas/$3)
+  if [ "$response" == "$expected" ]
   then
+    ((successTests++))
+  else
     echo "get-blur-areas: Test failed at image: $2"
     echo "Returned: $response"
     echo "Should return: $expected"
-    exit 1 
+    ((failTests++))
   fi
 }
 
@@ -29,3 +41,10 @@ check_image $1 "OnePerson.png" "OnePerson.txt"
 check_image $1 "GithubBranching.png" "GithubBranching.txt"
 check_image $1 "RandomText.txt" "RandomText.txt"
 check_image $1 "EmptyFile.txt" "EmptyFile.txt"
+
+totalTests=$((failTests+successTests))
+echo "Tests ran: $totalTests; Success: $successTests; Failed: $failTests"
+if [ $failTests -gt 0 ]
+then
+  exit 1
+fi
