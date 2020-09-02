e0cd0be64aef313b5d9c675803da30fa30880597
Aleksandra Bakalova <abakalova@google.com>
2020-09-02 20:29:18 +0300

    fix linter issues

--- src/main/webapp/get-blur-areas.js
+++ src/main/webapp/get-blur-areas.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "getBlurAreas" }] */
+
 'use strict';
 
 /**
