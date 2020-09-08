90b53d6f387e220d39a962168d87ab7e98782412
Aleksandra Bakalova <abakalova@google.com>
2020-09-08 13:34:23 +0300

    code style

--- src/main/webapp/blur.js
+++ src/main/webapp/blur.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// suppress linter error - getImageWithBlurredAreas function is used from another file
 /* eslint no-unused-vars:
 ["error", { "varsIgnorePattern": "getImageWithBlurredAreas" }] */
 
