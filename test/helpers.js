ed89df0ae79da0bbac25dd4d7355558f23c5ebdb
Aleksandra Bakalova <abakalova@google.com>
2020-09-08 13:49:49 +0300

    code style - comment about suppressing linter

--- test/helpers.js
+++ test/helpers.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// suppress linter error - getFileObject and
+// getImageFromPath functions are used from another file.
 /* eslint no-unused-vars:
 ["error", { "varsIgnorePattern": "getFileObject|getImageFromPath" }] */
 
