63a5896836885465dbe59bf126e8fe794a18f961
mpreda <mpreda@google.com>
2020-09-14 13:47:17 +0300

    Add copyright and supress linter errors.

--- src/main/webapp/blur-helpers.js
+++ src/main/webapp/blur-helpers.js
@@ -1,3 +1,22 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// suppress linter error - functions are used in another file.
+/* eslint no-unused-vars:
+["error", { "varsIgnorePattern":
+"getImageFromUrl|getDefaultBlurRadius|" }] */
+
 /**
  * Helper function to get average rect size of
  * rects to blur.
@@ -33,7 +52,7 @@ function getImageFromUrl(url) {
 
 /**
  * Helper function to get the default blurRadius for blurAreas.
- * @param {Array<Rect>} rects
+ * @param {Array<Rect>} blurAreas
  * @return {Number} default blur Radius.
  */
 function getDefaultBlurRadius(blurAreas) {
